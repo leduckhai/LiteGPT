d18628d1f8ccf5496957e0775b3d4869e30f21f3
Hanhpt23 <hanhpt.phamtan@gmail.com>
2024-08-08 21:47:59 -0400

    adding random audios

--- medlvlm/datasets/datasets/vindrcxr_dataset.py
+++ medlvlm/datasets/datasets/vindrcxr_dataset.py
@@ -1,6 +1,7 @@
 import os
 import json
 import random
+import torch
 
 from PIL import Image
 from torch.utils.data import Dataset
@@ -19,11 +20,11 @@ class VinDrCXRDataset(Dataset):
         if prompt_test is None:
             self.instruction_pool = [
                 "[grounding] please describe this image in details with radiological features. Use two sentences unless there are no findings. The first sentence should list the global diseases present in the image, and the second should list local diseases with localized bounding boxes.",
-                "[grounding] please describe this image in details with radiological features. Use two sentences unless there are no findings. The first sentence should list the global diseases present in the image, and the second should list local diseases with localized bounding boxes. Let's think step by step.",
-                "[grounding] This is a radiological image of a chest x-ray from a Posterior Anterior view. Examine all the areas where the lung borders the diaphragm, the heart and other mediastinal structures. Please describe this image in details with radiological features. Use two sentences unless there are no findings. The first sentence should list the global diseases present in the image, and the second should list local diseases with localized bounding boxes.",
-                "[grounding] This is a radiological image of a chest x-ray from a Posterior Anterior view. Examine all the areas where the lung borders the diaphragm, the heart and other mediastinal structures. Please describe this image in details with radiological features. Use two sentences unless there are no findings. The first sentence should list the global diseases present in the image, and the second should list local diseases with localized bounding boxes. Let's think step by step.",
-                "[grounding] Please describe this image in details with radiological features. Use one sentence unless there are no findings. The sentence should list the global diseases present in the image."
-                "[grounding] This is a radiological image of a chest x-ray from a Posterior Anterior view. Examine all the areas where the lung borders  the diaphragm, the heart and other mediastinal structures. Please describe this image in details with radiological features. Use one sentence unless there are no findings. The sentence should list the  global diseases present in the image."
+                # "[grounding] please describe this image in details with radiological features. Use two sentences unless there are no findings. The first sentence should list the global diseases present in the image, and the second should list local diseases with localized bounding boxes. Let's think step by step.",
+                # "[grounding] This is a radiological image of a chest x-ray from a Posterior Anterior view. Examine all the areas where the lung borders the diaphragm, the heart and other mediastinal structures. Please describe this image in details with radiological features. Use two sentences unless there are no findings. The first sentence should list the global diseases present in the image, and the second should list local diseases with localized bounding boxes.",
+                # "[grounding] This is a radiological image of a chest x-ray from a Posterior Anterior view. Examine all the areas where the lung borders the diaphragm, the heart and other mediastinal structures. Please describe this image in details with radiological features. Use two sentences unless there are no findings. The first sentence should list the global diseases present in the image, and the second should list local diseases with localized bounding boxes. Let's think step by step.",
+                # "[grounding] Please describe this image in details with radiological features. Use one sentence unless there are no findings. The sentence should list the global diseases present in the image."
+                # "[grounding] This is a radiological image of a chest x-ray from a Posterior Anterior view. Examine all the areas where the lung borders  the diaphragm, the heart and other mediastinal structures. Please describe this image in details with radiological features. Use one sentence unless there are no findings. The sentence should list the  global diseases present in the image."
             ]
         else:
             self.instruction_pool = [prompt_test]
@@ -44,11 +45,12 @@ class VinDrCXRDataset(Dataset):
         image = self.vis_processor(image)
 
         answer = info['grounded_diseases']
-        instruction = random.choice(self.instruction_pool)
+        # instruction = random.choice(self.instruction_pool)
         instruction = "<Img><ImageHere></Img> {} ".format(instruction)
 
         return {
             "image": image,
+            "audio": torch.rand(80, 3000, dtype=torch.float16),
             "instruction_input": instruction,
             "answer": answer,
             "image_id": info['image_id'],
